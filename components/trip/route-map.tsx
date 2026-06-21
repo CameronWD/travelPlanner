369f33c0a9bc17a95aba10f5b642f4df107b9a7a
Cam <cwilliams.developer@gmail.com>
2026-06-21 23:10:56 +0000

    chore(map): self-host Leaflet marker icons, drop unpkg CDN dependency

--- components/trip/route-map.tsx
+++ components/trip/route-map.tsx
@@ -118,9 +118,9 @@ export function RouteMap({ stops, height = 360 }: RouteMapProps) {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       delete (L.Icon.Default.prototype as any)._getIconUrl;
       L.Icon.Default.mergeOptions({
-        iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-        iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-        shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+        iconRetinaUrl: "/leaflet/marker-icon-2x.png",
+        iconUrl: "/leaflet/marker-icon.png",
+        shadowUrl: "/leaflet/marker-shadow.png",
       });
 
       if (!mapRef.current) return;
