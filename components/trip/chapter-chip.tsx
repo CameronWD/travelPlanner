e5330dc76ee7915893abca6627c91eb6a99349ae
Cam <cwilliams.developer@gmail.com>
2026-06-23 06:58:09 +0000

    feat(chapters): ChapterChip component

---
+++ components/trip/chapter-chip.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { cn } from "@/lib/cn";
+import { chapterColourMeta } from "@/lib/chapter-colours";
+
+interface ChapterChipProps extends React.HTMLAttributes<HTMLSpanElement> {
+  name: string;
+  colour: string;
+}
+
+export function ChapterChip({ name, colour, className, ...props }: ChapterChipProps) {
+  const meta = chapterColourMeta(colour);
+  return (
+    <span
+      className={cn(
+        "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-medium",
+        meta.chipClass,
+        className,
+      )}
+      {...props}
+    >
+      {name}
+    </span>
+  );
+}
